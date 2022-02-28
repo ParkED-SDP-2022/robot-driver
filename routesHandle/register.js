36c89cc1b7962ffeb44f036497813068ed279cec
229770716 <60184977+229770716@users.noreply.github.com>
2022-02-28 15:11:39 +0000

    f

--- routesHandle/register.js
+++ routesHandle/register.js
@@ -1,24 +1,32 @@
+const bcrypt = require('bcryptjs');
 const db = require('../db');
+
 
 module.exports = (req, res) => {
     //search if username exists
-    // const sql = 'SELECT * FROM user WHERE username=?';
-    // db(sql, req.body.username, result => {
-    //     if (result.length >= 1) {
-    //         return res.send({
-    //             status: 1,
-    //             msg: 'username already exists'
-    //         });
-    //     }
-    //     const sql = 'INSERT INTO user set ?';
-    //     const { username, email, password } = req.body;
-    //     db(sql, {username, email, password }, result => {
-    //         res.send(result);
-    //     });
-    //     res.send({
-    //         status: 0,
-    //         msg: 'successfully signed up'
-    //     });
-    // });
-    res.send(req.body);
+    const sql = 'SELECT * FROM user WHERE username=?';
+    db(sql, req.body.username, result => {
+        if (result.length >= 1) {
+            return res.send({
+                status: 1,
+                msg: 'username already exists'
+            });
+        }
+        const sql = 'INSERT INTO user set ?';
+        req.body.password = bcrypt.hashSync(req.body.password, 10);
+        const { username, email, password } = req.body;
+        db(sql, {username, email, password }, result => {
+            if (result.affectedRows === 1) {
+                return res.send({
+                    status: 0,
+                    msg: "successfully registered"
+                });
+            }
+            res.send({
+                status: 1,
+                msg: "failed registration"
+            });
+        });
+    });
+    // res.send(req.body);
 };
