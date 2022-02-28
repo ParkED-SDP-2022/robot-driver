4af2c47ffd781c3ef347f57e6883ae553aed0c51
MuizMohamad <muizzuddinmohamad@gmail.com>
2022-02-28 16:29:23 +0000

    test fix

--- routes/register.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const valid = require('../middleware/valid');
-const registerHandle = require('../routesHandle/register');
-const { registerSchema } = require('../schema/register');
-
-router.post('/', valid(registerSchema), registerHandle);
-
-module.exports = router; 
